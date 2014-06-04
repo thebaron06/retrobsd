c006ccb4d77ef3dc6f5df1e7517c089a482c3293
thebaron06 <thebaron06@gmail.com>
2014-06-04 20:27:24 +0200

    Switched to linaro toolchain because there is hard float support.

--- sys/stm32/gcc-config.mk
+++ sys/stm32/gcc-config.mk
@@ -1,13 +1,25 @@
+
+# Linaro toolchain
+# https://launchpad.net/gcc-arm-embedded/+milestone/4.6-2012-q4-update
+ifndef GCCPREFIX
+ifeq (/opt/linaro/arm-2013.11/bin/arm-none-linux-gnueabi-gcc,$(wildcard /opt/linaro/arm-2013.11/bin/arm-none-linux-gnueabi-gcc))
+	GCCPREFIX   = /opt/linaro/arm-2013.11/bin/arm-none-linux-gnueabi-
+	LDFLAGS     = -Wl,--oformat=elf32-littlearm
+endif
+endif
+
+
 # Mentor Sourcery CodeBench Lite toolchain
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # You can download a Linux or Windows binary package from
 # https://sourcery.mentor.com/
 ifndef GCCPREFIX
-    GCCPREFIX   = arm-none-linux-gnueabi-
-    LDFLAGS     = -Wl,--oformat=elf32-littlearm
+ifeq (/opt/CodeSourcery/2013.11-33/bin/arm-none-linux-gnueabi-gcc,$(wildcard /opt/CodeSourcery/2013.11-33/bin/arm-none-linux-gnueabi-gcc))
+	GCCPREFIX   = /opt/CodeSourcery/2013.11-33/bin/arm-none-linux-gnueabi-
+	LDFLAGS     = -Wl,--oformat=elf32-littlearm
+endif
 endif
 
 ifndef GCCPREFIX
     $(error Unable to locate any GCC ARM toolchain!)
 endif
-
